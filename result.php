a3f7aa79c4d8ee3b78655a486eb0ce52d8445c3c
mohan <singhmohan333@gmail.com>
2017-06-22 23:15:10 +0530

    New files

---
+++ result.php
@@ -0,0 +1,25 @@
+ <!DOCTYPE html>
+<html>
+<body>
+
+<h1>A Web Page</h1>
+<p id="demo">A Paragraph</p>
+<button type="button" onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+   document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
+
+<h1>My First Web Page</h1>
+<p>My First Paragraph</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = 5 + 6;
+</script>
+
+</body>
+</html> 
