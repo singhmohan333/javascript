93dcd0c1e304b85b07a4042bcd830bfd821c9de0
MOHAN <singhmohan333@gmail.com>
2017-07-04 14:39:54 +0530

    new angular directive...

---
+++ detailEmp.js
@@ -0,0 +1,37 @@
+angular.module('detailEmp', ['ngAnimate','ui.router'])
+
+    .config(function($stateProvider, $urlRouterProvider){
+     $stateProvider.state('home',
+      {
+      url: '/home',
+      template: 'helloooooo'
+    
+     });
+
+    })
+
+	.controller('myCntrl', function($scope)
+	{
+
+		 
+
+		 $scope.toggle = true ;
+
+			$scope.list = [ {name:'mohan',  age:20 , img:'il.png'},
+			{name:'MONU',  age:24 , img:'il.png'},
+			{name:'raju',  age:30 , img:'ik.png'},
+			{name:'ROHAN',  age:35 , img:'il.png'},
+			{name:'alon',  age:26 , img:'ik.png'},
+			{name:'sk',  age:74  , img:'ik.png'}
+			];
+     $scope.addPerson = function() {
+     	$scope.list.push({name: $scope.name, age: $scope.age});
+     	$scope.name = '';
+     	$scope.age = 0;
+     }
+	})
+	.directive('heloWeb', function(){
+		return {
+			template: 'Hello my web page'
+		}
+	});
